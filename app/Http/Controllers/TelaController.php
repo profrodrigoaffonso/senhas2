c7ac7fdb89e6539fb832487748b189642256d65a
Rodrigo Affonso <profrodrigoaffonso@gmail.com>
2021-06-15 13:28:59 -0300

    Atualizacao

--- app/Http/Controllers/TelaController.php
+++ app/Http/Controllers/TelaController.php
@@ -16,15 +16,15 @@ class TelaController extends Controller
     //
 
     public function master(){
-
+        // configuracoes
         $config = Configs::findOrFail(1);
-
         return view('tela.master', compact('config'));
     }
 
 
     public function chamar($guiche_id){
 
+        //seleciona a próxima senha disponível
         $senha = Senhas::select()
             ->where('em_uso', 'n')
             ->orderBy('id', 'ASC')
@@ -35,20 +35,21 @@ class TelaController extends Controller
             'guiche_id' => $guiche_id
         ];
 
+        // insere na tabela chamadas
         Chamadas::create($dados);
 
         $telaMaster = TelaMaster::findOrFail(1);
-
+        // atualiza a tabela tela_masters
         $telaMaster->update([
             'senha_id' => $senha->id,
             'guiche_id' => $guiche_id,
             'som' => 's'
         ]);
-
+        
+        // atualiza a senha disponível para em uso
         $senha->update([
             'em_uso' => 's'
         ]);        
-
 
         die();
 
@@ -74,14 +75,13 @@ class TelaController extends Controller
 
     function anteriores(){
 
+        // exibe as últimas 5 senhas chamadas
         $chamadas = Chamadas::select('senhas.senha', 'guiches.nome')
             ->join('senhas','chamadas.senha_id', '=', 'senhas.id')
             ->join('guiches','chamadas.guiche_id', '=', 'guiches.id')
             ->orderBy('chamadas.id', 'DESC')
             ->limit(5)
             ->get();
-
-        // dd($chamadas);
 
         
         return view('tela.anteriores', compact('chamadas'));
@@ -90,62 +90,52 @@ class TelaController extends Controller
 
     public function atual(){
 
+        // exibe na tela atual a última senha chamada e ou repetida
+
+        // retorna a última senha chamada com a condição que não foi exibida na tela master
         $telaMaster = Chamadas::select('chamadas.id AS chamada_id', 'senhas.senha', 'guiches.nome', 'chamadas.som')
             ->join('senhas','chamadas.senha_id', '=', 'senhas.id')
             ->join('guiches','chamadas.guiche_id', '=', 'guiches.id')
             ->where('exibe_master', 's')
             ->first();
         
-        if($telaMaster){
-            // dd($telaMaster->chamada_id);
-            // $telaMaster->update([
-            //     'exibe_master' => 'n'
-            // ]);
-
-            $tabela = Chamadas::findOrFail($telaMaster->chamada_id);
-
-            // dd($tabela); 
+        if($telaMaster){ // se encontrada
+         
+            // atualiza o registro para não exibir mais
+            $tabela = Chamadas::findOrFail($telaMaster->chamada_id);        
 
             $tabela->update([
                 'exibe_master' => 'n'
-            ]);
-
-            //dd('jjj');
-
+            ]);            
+            
+            // altera o som para não para apenas executar uma vez
             $som = $telaMaster->som;
-
             if($som == 's'){
                 $tabela->update([
                     'som' => 'n'
                 ]);               
             }
 
-        } else {
+        } else { // caso não encontre o registro na tabela chamadas, exibe o que está na tabela tela_masters
             $telaMaster = TelaMaster::select()
                 ->join('senhas','tela_masters.senha_id', '=', 'senhas.id')
                 ->join('guiches','tela_masters.guiche_id', '=', 'guiches.id')
                 ->where('tela_masters.id', 1)
                 ->first();
 
-            // dd($telaMaster);
-
+            // altera o som para não para apenas executar uma vez
             $som = $telaMaster['som'];
-
             if($som == 's'){
                 $telaMaster->update([
                     'som' => 'n'
                 ]);
             }
         }
-
+        // altera o som para não para apenas executar uma vez
         $tabela = TelaMaster::findOrFail(1);
-
         $tabela->update([
             'som' => 'n'
         ]);
-
-
-        //echo $som;
 
         return view('tela.atual', compact('telaMaster', 'som'));
     }
